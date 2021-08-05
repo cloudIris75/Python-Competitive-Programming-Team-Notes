287717e50420df962805904d8e95e75162ad0353
Gyeongmin Yeom <gmyeom1230@gmail.com>
2021-08-06 01:28:43 +0900

    Add files via upload

---
+++ List/maze_problem.py
@@ -0,0 +1,24 @@
+#CodeUp No.6098
+box = [list(map(int, input().split())) for _ in range(10)]
+
+# x : y-coordinate, y : x-coordinate
+x = 1
+y = 1
+
+while x<=9 and y<=9:
+  if box[x][y] == 0:
+    box[x][y] = 9
+    y += 1
+  elif box[x][y] == 1:
+    y -= 1
+    x += 1
+  elif box[x][y] == 2:
+    box[x][y] = 9
+    break
+  else:
+    break
+
+for i in range(10):
+  for j in range(10):
+    print(box[i][j], end=' ')
+  print()
