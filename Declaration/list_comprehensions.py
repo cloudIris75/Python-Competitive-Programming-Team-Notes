563bdad4e2946a4f3443356777109e7eb7ba48c8
Gyeongmin Yeom <gmyeom1230@gmail.com>
2021-08-05 14:24:55 +0900

    Add files via upload

---
+++ Declaration/list_comprehensions.py
@@ -0,0 +1,19 @@
+# List Comprehensions
+'''
+for i in range(20):
+  board.append([])
+  for j in range(20):
+    board[i].append(0)
+'''
+board = [[0 for j in range(20)] for i in range(20)]
+
+# Place a white stone on a go board
+n = int(input())
+for i in range(n):
+  x, y = map(int, input().split())
+  board[x][y] = 1
+
+for i in range(1, 20):
+  for j in range(1, 20):
+    print(board[i][j], end=' ')
+  print()
